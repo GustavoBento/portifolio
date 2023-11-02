228c3e62306462252c6c8a0d1a0a2f6a0655fb2c
Gustavo Bento <133028031+GustavoBento@users.noreply.github.com>
2023-11-02 10:32:43 -0300

    testes

--- menu.js
+++ menu.js
@@ -15,6 +15,6 @@ overlay.addEventListener('click', () => {
     menuMobile.classList.remove('abrir_menu')
 })
 
-inputEnviar.addEventListener('click', () => {
+inputEnviar.addEventListener('submit', () => {
     window.alert('Sua mensagem foi enviada!')
 })
